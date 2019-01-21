2a6434265772dca7f536ef09e2adf202e77cd542
galdinoRosas <galdino.rosas@walmartlabs.com>
2019-01-21 00:50:44 -0800

    test

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 {
     "github_username": "galdinorosas",
-    "github_webhook_path": "http://github-automerger-github-automerger.1d35.starter-us-east-1.openshiftapps.com/8080",
+    "github_webhook_path": "http://github-automerger-github-automerger.1d35.starter-us-east-1.openshiftapps.com",
     "github_webhook_secret": "passwordisdinomite",
     "delete_after_merge": true,
     "port": 8080
