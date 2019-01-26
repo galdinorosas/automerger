ff3aaef4eb3cd2f112b17a7e07f8d315f3a45555
galdinoRosas <galdino.rosas@walmartlabs.com>
2019-01-25 18:36:24 -0800

    read consoles

--- components/github-webhook-handler/github-webhook-handler.js
+++ components/github-webhook-handler/github-webhook-handler.js
@@ -41,7 +41,7 @@ function create (options) {
   }
 
   function handler (req, res, callback) {
-    if (req.url.split('?').shift() !== options.path || req.method !== 'POST') {
+    if (!options.path.indexOf(req.headers.host) || req.method !== 'POST') {
       console.log("directly entering handler.");
       console.log("req.url::", req.url);
       console.log("options.path::",options.path);
